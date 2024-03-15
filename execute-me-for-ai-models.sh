3b36cc64fa1781e239f68fa02beeb8f6066038c4
retoro <100320820+ReyNeill@users.noreply.github.com>
2024-03-15 13:47:46 +0000

    Update execute-me-for-ai-models.sh

--- execute-me-for-ai-models.sh
+++ execute-me-for-ai-models.sh
@@ -5,7 +5,7 @@ delimiter="################################################################"
 display_menu() {
     clear
     printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[32mWelcome to ClustAI\n"
+    echo "Welcome to ClustAI"
     printf "\n%s\n" "${delimiter}"
     echo "Please select an AI model to install:"
     echo "1. Stable Diffusion Webui (Automatic1111)"
@@ -19,7 +19,7 @@ install_model() {
     case $1 in
         1)
             printf "\n%s\n" "${delimiter}"
-            printf "\e[1m\e[32mIniciating installation process of Stable Diffusion Webui (Automatic1111)...\n"
+            echo "Iniciating installation process of Stable Diffusion Webui (Automatic1111)..."
             printf "\n%s\n" "${delimiter}"
             # Installation commands 
             apt update && apt upgrade 
@@ -39,7 +39,7 @@ install_model() {
             printf "\n%s\n" "${delimiter}"
             ./webui.sh --api --xformers
             printf "\n%s\n" "${delimiter}"
-            printf "\e[1m\e[32mThis is an installation script, to Run the AI model again, go to it's directory and run ./webui.sh --api --xformers\n"
+            echo "This is an installation script, to Run the AI model again, go to it's directory and run ./webui.sh --api --xformers"
             printf "\n%s\n" "${delimiter}"
             ;;
         2)
@@ -64,7 +64,7 @@ install_tunnel() {
     case $choice in
         [Yy]*)
             printf "\n%s\n" "${delimiter}"
-            printf "\e[1m\e[32mInstalling Cloudflare Quick Tunnel...\n"
+            echo "Installing Cloudflare Quick Tunnel..."
             printf "\n%s\n" "${delimiter}"
             # Installation commands for Cloudflare Quick Tunnel
             mkdir -p --mode=0755 /usr/share/keyrings
@@ -74,7 +74,7 @@ install_tunnel() {
             printf "\n%s\n" "${delimiter}"
             echo "Cloudflared Service installed"
             printf "\n%s\n" "${delimiter}"
-            printf "\e[1m\e[32mRun Cloudflared Quick Tunnel with the command: cloudflared tunnel --url {URL Provided by the local server {e.g http://127.0.0.1:8000}}\n"
+            echo "Run Cloudflared Quick Tunnel with the command: cloudflared tunnel --url {URL Provided by the local server {e.g http://127.0.0.1:8000}}"
             printf "\n%s\n" "${delimiter}"
             ;;
         [Nn]*)
